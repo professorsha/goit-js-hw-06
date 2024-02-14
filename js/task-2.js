982bc4eaf013424f0968b4ec15b831ac1905a570
professorsha <69015177+professorsha@users.noreply.github.com>
2024-02-14 08:04:57 +0200

    add task-1 task-2 task-3

---
+++ js/task-2.js
@@ -0,0 +1,23 @@
+// removeItem(itemToRemove) — приймає рядок з назвою товару itemToRemove і видаляє його з масиву
+// товарів у приватній властивості items об'єкта.
+class Storage {
+  #item;
+  constructor(item) {
+    this.#item = item;
+  }
+  getItems() {
+    return this.#item;
+  }
+  addItem(newItem) {
+    this.#item.push(newItem);
+  }
+  removeItem(itemToRemove) {
+    this.#item = this.#item.filter(item => item != itemToRemove);
+  }
+}
+const storage = new Storage(['Nanitoids', 'Prolonger', 'Antigravitator']);
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+storage.addItem('Droid');
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+storage.removeItem('Prolonger');
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
